67a786c00298b8e55ab1012293fad71612a378e1
pavani <pavanivn13@gmail.com>
2022-06-01 12:44:25 +0530

    page

---
+++ src/main/java/Batch/program/App.java
@@ -0,0 +1,13 @@
+package Batch.program;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
